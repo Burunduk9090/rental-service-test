ae7827578f058acc0c07f665a074833197bc3f19
Taras Sushko <burunduk9090@gmail.com>
2025-05-17 12:33:40 +0300

    Add models, add new files for api

--- backend/apps/apartments/models.py
+++ backend/apps/apartments/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Apartment(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    number_of_rooms = models.IntegerField()
+    square = models.DecimalField(max_digits=8, decimal_places=2)
+    availability = models.BooleanField(default=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='apartments'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from apps.users.models import User
+from apps.apartments.models import Apartment
 
 
 @admin.register(User)
@@ -8,7 +9,8 @@ class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['email', 'is_verified', 'is_staff', 'is_active', 'date_joined']
     list_filter = ['is_verified', 'is_staff', 'is_active']
     search_fields = ['email']
-    ordering = ['email']
+    ordering = ['is_active', 'email']
+    list_per_page = 25
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
@@ -21,5 +23,23 @@ class CustomUserAdmin(admin.ModelAdmin):
             'fields': ('email', 'password1', 'password2', 'is_verified', 'is_staff', 'is_active'),
         }),
     )
-    filter_horizontal = ()
-    ordering = ('email',)
+
+
+@admin.register(Apartment)
+class ApartmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'number_of_rooms', 'availability', 'owner', 'created_at', 'updated_at']
+    list_filter = ['availability', 'owner', 'price', 'number_of_rooms', 'created_at', 'updated_at']
+    search_fields = ['name', 'description', 'owner__email']
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ('-created_at',)
+    list_per_page = 25
+    readonly_fields = ('created_at', 'updated_at')
+
+    fieldsets = (
+        ('Main Information', {
+            'fields': ('name', 'slug', 'description', 'price', 'number_of_rooms', 'square', 'availability')
+        }),
+        ('Additional Information', {
+            'fields': ('owner', 'created_at', 'updated_at'),
+        }),
+    )
